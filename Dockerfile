9f6b8b73acaabf0c3adad3de4884298aa62e1062
anh4n <>
2024-11-28 22:43:31 +0100

    add git commit to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM node:18.16.0-alpine as builder
+ARG GIT_COMMIT
+ENV GIT_COMMIT=$GIT_COMMIT
 
 LABEL org.opencontainers.image.source=https://github.com/anh4n/skyjo
 LABEL org.opencontainers.image.description="Skyjo"
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import * as child from "child_process";
 
-const commitHash = child.execSync("git rev-parse --short HEAD").toString();
-
-// https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    __APP_VERSION__: JSON.stringify(commitHash),
+    __APP_VERSION__: JSON.stringify(process.env.GIT_COMMIT),
   },
   plugins: [svelte()],
 })
